77166a30510fbc82cb0d45759c2d887cd4697a20
Steven Klaiber-Noble <snoble@fragment.dev>
2025-02-04 18:48:05 +0000

    run gen scripts

--- dist/cjs/types/generated/version.d.ts
+++ dist/cjs/types/generated/version.d.ts
@@ -1,5 +1,5 @@
 /**
  * A string representing the current version of the package.
  */
-export declare const version: "1.0.6";
+export declare const version: "1.0.7";
 //# sourceMappingURL=version.d.ts.map
