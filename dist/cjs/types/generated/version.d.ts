3dc91b448771b85e5051ff23789a8a62327a053a
jkvlevin <jkvlevin@users.noreply.github.com>
2025-07-24 15:31:18 +0000

    [create-pull-request] automated change

--- dist/cjs/types/generated/version.d.ts
+++ dist/cjs/types/generated/version.d.ts
@@ -1,5 +1,5 @@
 /**
  * A string representing the current version of the package.
  */
-export declare const version: "1.3.0";
+export declare const version: "1.3.1";
 //# sourceMappingURL=version.d.ts.map
