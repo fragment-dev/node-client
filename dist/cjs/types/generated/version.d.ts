c9a03aa58030a8d72209a47a57c80888a77b918e
dggrunzweig <dggrunzweig@users.noreply.github.com>
2025-12-17 23:01:17 +0000

    [create-pull-request] automated change

--- dist/cjs/types/generated/version.d.ts
+++ dist/cjs/types/generated/version.d.ts
@@ -1,5 +1,5 @@
 /**
  * A string representing the current version of the package.
  */
-export declare const version: "1.3.4";
+export declare const version: "1.4.0";
 //# sourceMappingURL=version.d.ts.map
