d90f5c34132396ad967aabccdad6fbb3b4dae8ea
snoble <snoble@users.noreply.github.com>
2026-03-30 19:12:18 +0000

    [create-pull-request] automated change

--- dist/esm/generated/version.js
+++ dist/esm/generated/version.js
@@ -2,4 +2,4 @@
 /**
  * A string representing the current version of the package.
  */
-export const version = '1.5.0';
+export const version = '1.5.1';
--- dist/esm/types/generated/version.d.ts
+++ dist/esm/types/generated/version.d.ts
@@ -1,5 +1,5 @@
 /**
  * A string representing the current version of the package.
  */
-export declare const version: "1.5.0";
+export declare const version: "1.5.1";
 //# sourceMappingURL=version.d.ts.map
